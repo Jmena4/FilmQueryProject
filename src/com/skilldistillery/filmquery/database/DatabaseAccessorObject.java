621f9d67673024f1229238bc1c5364f926e85fb4
Jesse <Jesse.mena4@gmail.com>
2019-02-15 19:23:24 -0700

    Update: updated the AccessorObject

--- src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -1,5 +1,8 @@
 package com.skilldistillery.filmquery.database;
 
+import java.util.List;
+
+import com.skilldistillery.filmquery.entities.Actor;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class DatabaseAccessorObject implements DatabaseAccessor {
@@ -8,5 +11,14 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
   public Film findFilmById(int filmId) {
     return null;
   }
+  @Override
+  public Actor findActorById(int actorId) {
+	  return null;
+  }
+  @Override
+  public List findActorsByFilmId(int ResultSet) {
+	  return null;
+  }
+  
 
 }

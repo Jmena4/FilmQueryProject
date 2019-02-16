a4fcb459a060ecb4554d65b0d5c63da2496292f7
Jesse <Jesse.mena4@gmail.com>
2019-02-15 19:20:34 -0700

    Update: added new features to the FilmQueryApp

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -1,5 +1,10 @@
 package com.skilldistillery.filmquery.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import com.skilldistillery.filmquery.database.DatabaseAccessor;
@@ -7,30 +12,53 @@ import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class FilmQueryApp {
-  
-  DatabaseAccessor db = new DatabaseAccessorObject();
+	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
+	DatabaseAccessor db = new DatabaseAccessorObject();
 
-  public static void main(String[] args) {
-    FilmQueryApp app = new FilmQueryApp();
-    app.test();
-//    app.launch();
-  }
+	public FilmQueryApp() throws ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+	}
 
-  private void test() {
-    Film film = db.findFilmById(1);
-    System.out.println(film);
-  }
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		System.out.println("Film Query App started.");
+		FilmQueryApp app = new FilmQueryApp();
+		app.test();
+		app.launch();
+	}
 
-  private void launch() {
-    Scanner input = new Scanner(System.in);
-    
-    startUserInterface(input);
-    
-    input.close();
-  }
+	private void test() {
+		Film film = db.findFilmById(1);
+		System.out.println(film);
+	}
 
-  private void startUserInterface(Scanner input) {
-    
-  }
+	private void launch() throws SQLException {
+		Scanner input = new Scanner(System.in);
+
+		startUserInterface(input);
+
+		input.close();
+	}
+
+	private void startUserInterface(Scanner input) throws SQLException {
+		String user = "student";
+		String pass = "student";
+		Connection conn = DriverManager.getConnection(URL, user, pass);
+		String sqltxt;
+		sqltxt = "SELECT id, first_name, last_name FROM staff order by first_name";
+		PreparedStatement stmt = conn.prepareStatement(sqltxt);
+		ResultSet rs = stmt.executeQuery(sqltxt);
+
+		int counter = 1;
+
+		while (rs.next()) {
+			System.out.println(counter + " " + rs.getString("id") + " " + rs.getString("first_name") + " "
+					+ rs.getString("last_name"));
+			counter++;
+		}
+		rs.close();
+		stmt.close();
+		conn.close();
+
+	}
 
 }
